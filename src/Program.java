8503f01c2a6cd8489f1e14a231d45eb7a3aa39f9
Sher009 <sherlok019@gmail>
2024-01-14 19:13:14 +0100

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Program {
+
+        public static void main(String[]args){
+                 Scanner scanner = new Scanner(System.in);
+
+                    System.out.println("Введіть перше число");
+                    float a = scanner.nextFloat();
+
+                    System.out.println("Введіть друге число");
+                    float b = scanner.nextFloat();
+
+                    System.out.println("Виберіть операцію: \n +  -  *  /");
+                    char operation = scanner.next().charAt(0);
+
+
+                 if (operation == '+'){
+                         float result = a + b;
+                         System.out.println( a + " + " + b + " = " + result);
+
+                  } else if (operation == '-') {
+                         float result = a - b;
+                         System.out.println( a + " - " + b + " = " + result);
+                 } else if (operation == '*' ) {
+                         float result = a * b;
+                     System.out.println(a + " * " + b + " = " + result);
+                 } else if (operation == '/' ) {
+                     float result = a / b;
+                     System.out.println(a + " / " + b + " = " + result);
+                     if (b == 0){
+                         System.out.println("Ділення на 0 неможливе");
+                     }
+
+
+
+                 }
+        }
+
+}
